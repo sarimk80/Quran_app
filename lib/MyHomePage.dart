4b542a6b08838a664dff5a2674416217c5059257
sarim <sarimk80>
2019-03-28 17:58:21 +0500

    Initial commit

---
+++ lib/MyHomePage.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'model/Chapters.dart';
+import 'model/bloc/HomePageBloc.dart';
+import 'package:quran_app/src/chapters/data.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'utils/searchbar.dart';
+
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _bloc = HomePagebloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: _bloc.output,
+      initialData: List<Chapters>(),
+      builder: (BuildContext context, AsyncSnapshot<List<Chapters>> snapshot) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+            leading: Icon(FontAwesomeIcons.mosque),
+            actions: <Widget>[
+              IconButton(
+                  icon: Icon(FontAwesomeIcons.search),
+                  onPressed: () {
+                    showSearch(context: context, delegate: SearchBar(snapshot.data));
+                  })
+            ],
+          ),
+          body: ListView(
+            children: snapshot.data.map(_buildItem).toList(),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            mini: false,
+            child: Icon(FontAwesomeIcons.quran),
+            tooltip: "Resume",
+
+          ),
+        );
+      },
+    );
+//    Scaffold(
+//      appBar: AppBar(
+//        title: Text(widget.title),
+//        leading: Icon(FontAwesomeIcons.mosque),
+//        actions: <Widget>[
+//          IconButton(
+//              icon: Icon(FontAwesomeIcons.search),
+//              onPressed: () {
+//                showSearch(context: context, delegate: SearchBar());
+//              })
+//        ],
+//      ),
+//      body: StreamBuilder(
+//          stream: _bloc.output,
+//          initialData: List<Chapters>(),
+//          builder:
+//              (BuildContext context, AsyncSnapshot<List<Chapters>> snapshot) {
+//            if (snapshot.connectionState == ConnectionState.waiting) {
+//              return Center(
+//                child: CircularProgressIndicator(
+//                  backgroundColor: Colors.limeAccent,
+//                ),
+//              );
+//            } else {
+//              return ListView(
+//                children: snapshot.data.map(_buildItem).toList(),
+//              );
+//            }
+//          }),
+//      floatingActionButton: FloatingActionButton(
+//        onPressed: () {},
+//        mini: false,
+//        child: Icon(FontAwesomeIcons.quran),
+//        tooltip: "Resume",
+//        backgroundColor: Colors.limeAccent,
+//      ),
+//    );
+//    // This trailing comma makes auto-formatting nicer for build methods.
+  }
+
+  Widget _buildItem(Chapters chap) {
+    return Padding(
+        padding: EdgeInsets.all(8),
+        child: Container(
+          decoration: BoxDecoration(
+              border: Border(
+                  bottom: BorderSide(color: Colors.white.withOpacity(0.5)))),
+          child: ListTile(
+            title: Text('${chap.name_simple}'),
+            subtitle: Text('${chap.revelation_place}'),
+            trailing: Text(
+              '${chap.name_arabic}',
+            ),
+            leading: Text('${chap.chapter_number}'),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => Data(
+                            name: chap.name_simple,
+                            id: chap.id,
+                          )));
+            },
+          ),
+        ));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _bloc.dispose();
+  }
+}
---
+++ lib/utils/searchbar.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:quran_app/model/Chapters.dart';
+import 'package:quran_app/src/chapters/data.dart';
+
+class SearchBar extends SearchDelegate<Chapters> {
+  var _chap = <Chapters>[];
+
+  SearchBar(this._chap);
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+          icon: Icon(Icons.close),
+          onPressed: () {
+            query = "";
+          })
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    final result =
+        _chap.where((a) => a.name_simple.contains(query));
+
+    return ListView(
+      children: result
+          .map<ListTile>((a) => ListTile(
+                title: Text(a.name_simple),
+                subtitle: Text(a.revelation_place),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => Data(
+                                name: a.name_simple,
+                                id: a.id,
+                              )));
+                },
+              ))
+          .toList(),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final result =
+        _chap.where((a) => a.name_simple.toLowerCase().contains(query));
+
+    return ListView(
+      children: result
+          .map<ListTile>((a) => ListTile(
+                title: Text(a.name_simple),
+                subtitle: Text(a.revelation_place),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => Data(
+                                name: a.name_simple,
+                                id: a.id,
+                              )));
+                },
+              ))
+          .toList(),
+    );
+  }
+}
